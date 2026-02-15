b8a639fdbdbd79cb9a5d66c297b318e19c7bbc48
Ewerton Siqueira <ewertoncostaxx@hotmail.com>
2026-02-15 14:47:34 -0300

    adicionando tema dark

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,23 +1,17 @@
 <template>
-  <v-app-bar
-    app
-    class="border-b text-center"
-    dark
-    density="comfortable"
-    flat
-  >
+  <v-app-bar app class="border-b text-center" dark density="comfortable" flat>
     <v-toolbar-title>E-commerce Tabajara</v-toolbar-title>
 
     <template #append>
-      <!-- <v-btn text>Home</v-btn>
-      <v-btn text>Produtos</v-btn>
-      <v-btn text>Contato</v-btn> -->
       <v-badge color="success" content="99+" location="top right">
         <v-icon icon="mdi-cart" />
       </v-badge>
+      <v-btn @click="theme.toggle()" icon="mdi-palette"></v-btn>
     </template>
   </v-app-bar>
 </template>
 <script setup>
+import { useTheme } from "vuetify";
 
+const theme = useTheme();
 </script>
